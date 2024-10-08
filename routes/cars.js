66a912bacb6365e6d17102e674242e7628be037a
raflytch <raflyazizabdillah30@gmail.com>
2024-10-08 15:18:44 +0700

    feat: make controller cars and motorcycles and controller createMotorcycles and routes cars and motorcycles

---
+++ routes/cars.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const carsController = require("../controller/cars");
+
+// Buat Router disini lalu gunakan callbacknya dari controller
+
+module.exports = router;
