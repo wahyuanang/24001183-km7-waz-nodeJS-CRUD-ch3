8a42107c7784d2566665db10c3362b5594ee8853
wahyuanang <wahyuanng@students.amikom.ac.id>
2024-10-08 16:07:29 +0700

    feat: create at update cars function challenge 3 by wahyu

--- controller/cars.js
+++ controller/cars.js
@@ -7,7 +7,51 @@ const { Cars } = require("../models");
 // CREATE CARS = POST
 
 // UPDATE CARS = PATCH
+const updateCars = async (req, res) => {
+  const { body, params } = req;
+  const { id } = params;
+  const updateAt = new Date();
+
+  try {
+    const data = await Cars.update(
+      {
+        ...body,
+        updateAt,
+      },
+      {
+        where: {
+          id,
+        },
+      }
+    );
+
+    if (!data) {
+      return res.status(400).json({
+        status: "Bad request",
+        message: "Invalid request",
+        error: "Invalid request",
+      });
+    }
+
+    res.status(200).json({
+      status: "OK",
+      message: "Car updated successfully",
+      data: {
+        ...body,
+        updateAt,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "Internal server error",
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
 
 // DELETE CARS = DELETE
 
-module.exports = {};
+module.exports = {
+  updateCars,
+};
--- routes/cars.js
+++ routes/cars.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const carsController = require("../controller/cars");
 
 // Buat Router disini lalu gunakan callbacknya dari controller
+// update
+router.patch("/:id", carsController.updateCars);
 
 module.exports = router;
