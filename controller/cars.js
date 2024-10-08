9fa566299a7ae36f2d03c83642cbe94f5dd2c314
JetroSulthan <jetrosulthan9626@gmail.com>
2024-10-08 19:10:37 +0700

    feat: read all cars and read cars by id

--- controller/cars.js
+++ controller/cars.js
@@ -1,6 +1,69 @@
 const { Cars } = require("../models");
 
 // GET ALL CARS = GET
+const readCars = async (req, res) => {
+    try {
+        const data = await Cars.findAll();
+
+        if (data === null) {
+            return res.status(400).json({
+                status : "Bad Request",
+                message : "Invalid Request",
+                isSucces : false,
+                Error : "Invalid Request"
+            })  
+        }
+
+        res.status(200).json({
+            status : "succes",
+            message : "Succes get cars data",
+            isSucces : true,
+            data : [
+                data
+            ]
+        })    
+    } catch (error) {
+        res.status(500).json({
+            status : "succes",
+            message : "Succes get cars data",
+            isSucces : true,
+            data : null
+        })    
+    }
+};
+
+const readCarsById = async (req, res) => {
+    try {
+        const CarsId = req.params.id;
+        const data = await Cars.findByPk(CarsId);
+
+        if (data === null) {
+            return res.status(400).json({
+                status : "Bad Request",
+                message : "Invalid Request",
+                isSucces : false,
+                Error : "Invalid Request"
+            })  
+        }
+
+        res.status(200).json({
+            status : "succes",
+            message : "Succes get cars data",
+            isSucces : true,
+            data : [
+                data
+            ]
+        })    
+    } catch (error) {
+        res.status(500).json({
+            status : "succes",
+            message : "Succes get cars data",
+            isSucces : true,
+            data : null
+        })    
+    }
+};
+  
 
 // GET CARS BY ID = GET
 
@@ -10,4 +73,7 @@ const { Cars } = require("../models");
 
 // DELETE CARS = DELETE
 
-module.exports = {};
+module.exports = {
+    readCars,
+    readCarsById
+};
--- routes/cars.js
+++ routes/cars.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const carsController = require("../controller/cars");
 
 // Buat Router disini lalu gunakan callbacknya dari controller
+router.get("/", carsController.readCars);
+router.get("/:id/", carsController.readCarsById);
 
 module.exports = router;
