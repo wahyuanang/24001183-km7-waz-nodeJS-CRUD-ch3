70f586a116df546e94513835930acfdfd567206b
NeoBitose <ramadhanalif430@gmail.com>
2024-10-08 15:50:05 +0700

    feat: add delete car function

--- routes/cars.js
+++ routes/cars.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const carsController = require("../controller/cars");
 
 // Buat Router disini lalu gunakan callbacknya dari controller
+router.delete("/:id", carsController.deleteCars);
 
 module.exports = router;
